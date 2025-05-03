5c417f43fba585b623ef21b4a9f90d37c7136bf5
StefanieFanie <stefanie_2226240009@mhs.mdp.ac.id>
2025-05-03 11:39:47 +0700

    ADD : controller produk dan migration

--- app/Livewire/Produk/Produk.php
+++ app/Livewire/Produk/Produk.php
@@ -3,11 +3,24 @@
 namespace App\Livewire\Produk;
 
 use Livewire\Component;
+use App\Models\produk as ProdukModel;
 
 class Produk extends Component
 {
+    public $produkList = [];
+
+    public function mount() {
+        $this->loadProduk();
+    }
+
+    public function loadProduk() {
+        $this->produkList = ProdukModel::all();
+    }
+
     public function render()
     {
-        return view('livewire.produk.produk');
+        return view('livewire.produk.produk', [
+            'list_produk' => $this->produkList
+        ]);
     }
 }
