8b7d4563161f0dd5ce43475981ead4dfd906fc65
StefanieFanie <stefanie_2226240009@mhs.mdp.ac.id>
2025-05-11 16:41:06 +0700

    Update ProdukController.php

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Produk;
-use App\Models\Kategori;
+use App\Models\kategori;
 
 class ProdukController extends Controller
 {
