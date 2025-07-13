a69cb79ad6832848b32f74c0863d17b89def38b5
StefanieFanie <stefanie_2226240009@mhs.mdp.ac.id>
2025-07-13 10:54:36 +0700

    UPDATE : input type cari ID penjualan

--- resources/views/laporan/laporan-penjualan.blade.php
+++ resources/views/laporan/laporan-penjualan.blade.php
@@ -94,7 +94,7 @@
     <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="#3B4B7A" class="bi bi-search" viewBox="0 0 16 16">
         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
     </svg>
-    <input type="text" id="cari" name="cari" class="form-control form-control-search me-2" placeholder="Cari ID penjualan..." value="{{ isset($cari) ? $cari : '' }}" style="height: 49px;">
+    <input type="number" id="cari" name="cari" class="form-control form-control-search me-2" placeholder="Cari ID penjualan..." value="{{ isset($cari) ? $cari : '' }}" style="height: 49px;">
 </form>
 <div class="table-responsive">
     <table class="table table-bordered border-secondary table-sm">
